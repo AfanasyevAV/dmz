3e24f8c315ea001086dafa1dc349634478ef1cb3
AfanasyevAV <kakjeo@mail.ru>
2022-06-09 01:27:00 +0300

    sec

---
+++ app/src/main/java/com/mrz/a123/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mrz.a123;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
